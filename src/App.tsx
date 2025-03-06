3b619ccd9c8ba6d8381d4a885ea81c81af693fac
Mrrob22 <54208557+Mrrob22@users.noreply.github.com>
2025-03-06 16:49:38 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RecipesPage from './pages/RecipePage';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import RecipePage from './pages/RecipePage';
 import SelectedRecipesPage from './pages/SelectedRecipesPage';
+import AllRecipesPage from "./pages/AllRecipesPage";
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
     return (
-        <Router basename="/receipes">
-            <div>
-                <h1>Рецепты</h1>
-                <Routes>
-                    <Route path="/" element={<RecipesPage />} />
-                    <Route path="/recipe/:id" element={<RecipePage />} />
-                    <Route path="/selected" element={<SelectedRecipesPage />} />
-                </Routes>
-            </div>
-        </Router>
+        <QueryClientProvider client={queryClient}>
+            <Router basename="/receipes">
+                <div>
+                    <h1>Рецепты</h1>
+                    <Routes>
+                        <Route path="/" element={<AllRecipesPage />} />
+                        <Route path="/recipe/:id" element={<RecipePage />} />
+                        <Route path="/selected" element={<SelectedRecipesPage />} />
+                    </Routes>
+                </div>
+            </Router>
+        </QueryClientProvider>
     );
 };
 
